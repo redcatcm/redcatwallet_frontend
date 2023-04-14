3a662f3c5c3ca20a7f6204d0e3c8c133ec2c59b7
oborshch <a.borsch@ukr.net>
2023-04-14 23:54:47 +0300

    upload frontend

---
+++ main.js
@@ -0,0 +1,17 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".column, .row, .about, .img__open, .about__text, .column__img, .partners__txt, .become__partner");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
