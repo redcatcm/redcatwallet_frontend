852c39131dff9b30f6a40b7f80a7ff3b31b8290c
oborshch <a.borsch@ukr.net>
2023-04-22 11:19:11 +0300

    mail sender

--- send_email.php
+++ send_email.php
@@ -1,14 +1,15 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
+  $telegram = $_POST["telegram"];
+  $mail = $_POST["mail"];
+  $link = $_POST["link"];
+  $project_description = $_POST["project_description"];
   
   $to = "wallet@redcat.click";
-  $subject = "Повідомлення з форми зворотнього зв'язку";
+  $subject = "Become partner";
   
   // Змінна для зберігання тексту листа
-  $body = "Ім'я: $name\nEmail: $email\nПовідомлення:\n$message";
+  $body = "Telegram: $telegram\nEmail: $mail\nLink:\n$link\nProject description: $project_description";
   
   // Встановлюємо заголовки листа
   $headers = "From: sender@example.com\r\n";
@@ -18,9 +19,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (mail($to, $subject, $body, $headers)) {
     http_response_code(200); // Встановлюємо статус 200 OK
     echo "The letter was sent successfully!";
+    header('Location: index.html');
   } else {
     http_response_code(500); // Встановлюємо статус 500 Internal Server Error
     echo "There was an error sending the email. Please try again later.";
+    header('Location: index.html');
   }
 }
 ?>
